4243c579d5ca244d8580e939e27bfa7480b40b37
Rajaeelazi <73946548+Rajaeelazi@users.noreply.github.com>
2020-11-29 23:40:34 +0000

    Add files via upload

---
+++ ExQCM.java
@@ -0,0 +1,15 @@
+public class ExQCM 
+{
+public static void main(String args[]) 
+{
+int x =0 , y=10;
+try {
+y /=x;
+ }
+ //system.out.print("/by 0");
+ catch (Exeption e){
+	  system.out.print("error");
+
+    }
+ }
+)	
